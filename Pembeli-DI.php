eb98cb9e0297bd2a751d7b773c3c8194d4677700
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2014-09-08 10:44:07 +0700

    Sample dengan dependency injection

--- Pembeli-DI.php
+++ Pembeli-DI.php
@@ -2,17 +2,17 @@
 include "BNI.php";
 
 class Pembeli {
-    private $nama;
-    private $bni;
+	private $nama;
+	private $bni;
 
-    public function __construct($nama = "Seseorang", BNI $bni) {
-        $this->bni = $bni;
-        $this->nama = $nama;
-    }
+	public function __construct($nama = "Seseorang", BNI $bni) {
+		$this->bni = $bni;
+		$this->nama = $nama;
+	}
 
-    public function beli($nama = "Barang", $harga = 0) {
-        $this->bni->kredit($harga);
-        echo "Berhasil melakukan pembelian $nama seharga Rp$harga.\n";
-        echo "Terima kasih $this->nama :)\n";
-    }
+	public function beli($nama = "Barang", $harga = 0) {
+		$this->bni->kredit($harga);
+		echo "Berhasil melakukan pembelian $nama seharga Rp$harga.\n";
+		echo "Terima kasih $this->nama :)\n";
+	}
 }
